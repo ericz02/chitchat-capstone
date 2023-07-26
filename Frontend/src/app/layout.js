7bcc99d60d1990e878d93c1e9ff9e03092878665
reo113 <roberteliortiz@gmail.com>
2023-07-26 16:29:21 -0400

    fixed the issue with the navbar and side bar

--- Frontend/src/app/layout.js
+++ Frontend/src/app/layout.js
@@ -1,27 +1,31 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Sidebar from "@/components/Sidebar";
 
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
-import Sidebar from '@/components/Sidebar'
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'ChitChat',
-  description: 'A forum based social media platform.',
-}
+  title: "ChitChat",
+  description: "A forum based social media platform.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <Sidebar />
-        {children}
-
+        <div className="flex">
+          <Sidebar />
+          <div className="flex flex-col w-full"> 
+          <Navbar />
+          <div className="flex flex-col flex-1">
+              {children}
+            </div>
+          </div>
+        </div>
         <Footer />
       </body>
     </html>
-  )
+  );
 }
