eaeb72b72b8a5345487534346d2f7872dfc10c34
Eric Zheng <84480698+ericz02@users.noreply.github.com>
2023-07-27 19:52:27 +0000

    fixed

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ const Profile = () => {
 
   return (
     <div>
-      <div className="bg-[#FFFFFF] p-4 flex flex-col justify-center ml-64 mt-20">
+      <div className="bg-[#FFFFFF] p-4 flex flex-col justify-center">
 
         
       </div>
--- Frontend/src/pages/Settings.jsx
+++ Frontend/src/pages/Settings.jsx
@@ -1,7 +1,7 @@
 const Settings = () => {
 
   return (
-    <div className="bg-[#FFFFFF] p-4 flex flex-col justify-center ml-64 mt-20">
+    <div className="bg-[#FFFFFF] p-4 flex flex-col justify-center">
       this is the settings page
     </div>
   )
