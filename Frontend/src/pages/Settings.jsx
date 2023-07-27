eb7102e83dd9042a0e37d3031b7ef48949eed7e0
ericz02 <ezheng0720@gmail.com>
2023-07-27 15:47:50 -0400

    adds settings page

---
+++ Frontend/src/pages/Settings.jsx
@@ -0,0 +1,10 @@
+const Settings = () => {
+
+  return (
+    <div className="bg-[#FFFFFF] p-4 flex flex-col justify-center ml-64 mt-20">
+      this is the settings page
+    </div>
+  )
+}
+
+export default Settings;
