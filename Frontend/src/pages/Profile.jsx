b0bfd252cc3dcb43dc59b092609ac8dfb92c1a41
ericz02 <ezheng0720@gmail.com>
2023-07-27 14:00:40 -0400

    adds profile, post, create page

---
+++ Frontend/src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+const Profile = () => {
+
+  return (
+    <div>
+      <div className="bg-[#FFFFFF] p-4 flex flex-col justify-center ml-64 mt-20">
+        profile page
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
