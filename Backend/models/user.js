6f12c4f3d5d9bd31aed8c05ecaf6f3662e357838
reo113 <roberteliortiz@gmail.com>
2023-07-31 14:07:06 -0400

    made changes to the relationships between the models

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -8,7 +8,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.hasMany(models.UserChatRelation);
+      this.belongsToMany(models.Chatroom, { through: models.UserChatRelation });
     }
   }
   User.init(
