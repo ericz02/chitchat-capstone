79e854a3931ba9967ab95426ba282953a04ca52a
ericz02 <ezheng0720@gmail.com>
2023-08-01 14:01:53 -0400

    adds post requests

--- Backend/server.js
+++ Backend/server.js
@@ -5,19 +5,47 @@ const port = 4000;
 const { Post } = require("./models");
 require("dotenv").config();
 
+// Welcome message for the root route of the serve
 app.get("/", (req, res) => {
   res.send("Welcome to ChitChat!");
 });
 
-const getPost = async (id) => {
-  const post = await Post.findByPk(parseInt(id, 10), { include: [Comment] });
-
-  if (!post) {
-    throw new NotFoundError("Post not found");
-  }
-  return post;
-};
-
+// Server listening on port 4000 for requests from the client
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
+});
+
+// Route to get all posts from the database
+app.get("/posts", async (req, res) => {
+
+  try {
+    const allPosts = await Post.findAll({ include: [Comment] });
+
+    res.status(200).json(allPosts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: err.message });
+  }
+  
+});
+
+//Get a specific post
+app.get("/posts/:id", async (req, res) => {
+  const postId = parseInt(req.params.id, 10);
+
+  try {
+    const post = await Post.findOne({
+      where: { id: postId },
+      include: [Comment],
+    });
+
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).send({ message: "Post not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: err.message });
+  }
 });
