bd5310cd99919a62af1c67232f4cfe05b1646735
Felix Chen <Felixx.Chenn@gmail.com>
2023-08-01 11:30:22 -0400

    Fix migrations and models

--- Backend/migrations/20230727153521-create-post.js
+++ Backend/migrations/20230727153521-create-post.js
@@ -30,7 +30,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         references: {
           model: "chatrooms",
-          key: "id",
+          key: "ChatroomId",
         },
         allowNull: false,
       },
