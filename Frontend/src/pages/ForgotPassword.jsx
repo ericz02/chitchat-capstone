b67fa9061511bece2bdd65364600f1da3039f0d5
ericz02 <ezheng0720@gmail.com>
2023-08-02 10:38:44 -0400

    fix forgot page

--- Frontend/src/pages/ForgotPassword.jsx
+++ Frontend/src/pages/ForgotPassword.jsx
@@ -16,29 +16,30 @@ const ForgotPassword = () => {
       </Link>
       <div className="  bg-white mx-52 mb-20 px-24 py-10 ">
         <div>
-            <h2 className="flex justify-center text-2xl font-bold mb-4 mt-3">
-                Forgot Password?
-            </h2>
-            <p className="text-sm text-slate-500 text-center">No worries, we'll email you reset instructions.</p>
-            <div>
-                <input
-                className="w-full h-10  border rounded bg-[#E6E6E6] mt-3 p-2"
-                type="email"
-                placeholder="Email: "
-                />
-            </div>
-            <div className=" flex flex-col p-4 ">
+          <h2 className="flex justify-center text-2xl font-bold mb-4 mt-3">
+            Forgot Password?
+          </h2>
+          <p className="text-sm text-slate-500 text-center">
+            No worries, we'll email you reset instructions.
+          </p>
+          <div>
+            <input
+              className="w-full h-10  border rounded bg-[#E6E6E6] mt-3 p-2"
+              type="email"
+              placeholder="Email: "
+            />
+          </div>
+          <div className=" flex flex-col p-4 ">
             <div className="flex justify-center">
               <button className=" bg-[#14AE5C] hover:bg-[#0F8B49] text-white font-bold py-2 px-4 rounded mx-3 w-1/2 my-1">
                 Submit
               </button>
             </div>
-            </div>
-        </div>   
+          </div>
+        </div>
       </div>
-      
     </div>
   );
-
+};
 
 export default ForgotPassword;
