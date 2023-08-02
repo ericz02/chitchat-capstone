6edc93ece80c27c8aa9a013f876384dd128ef186
reo113 <92471558+reo113@users.noreply.github.com>
2023-08-02 14:11:11 -0400

    Revert "finally fixed this modal issue"

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,22 +1,23 @@
 import Link from "next/link";
 
 const HomePage = () => {
+
   return (
     <div className="mx-auto max-w-4xl">
       <div className="flex flex-col justify-center mb-4">
         <div className="mx-auto">
           <Link href="/create">
-            <button className="bg-[#5175a8] px-4 py-2 rounded-[10px] hover:bg-[#526D82] transition-colors 
-              duration-300 ease-in-out mt-8 text-white"
+          <button
+            className="bg-[#27374D] px-4 py-2 rounded-[10px] hover:bg-[#526D82] transition-colors duration-300 ease-in-out mt-8"
             >
-              Create Post
-            </button>{" "}
+            Create Post
+          </button>{" "}
           </Link>
         </div>
       </div>
       {/* {postCards} */}
     </div>
-  );
-};
+  )
+}
 
 export default HomePage;
