7e4b8e7ff31f6b6570b23c90682b0092d5f23e6f
reo113 <roberteliortiz@gmail.com>
2023-08-04 00:45:23 -0400

    had to move viewpost to pages.post[id].js to get it to work with next.js but causing errors with the other routes

--- Frontend/src/pages/post/[id].js
+++ Frontend/src/pages/post/[id].js
@@ -1,9 +1,55 @@
-"use client";
 import RootLayout from "@/app/layout";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
+import { FaUserCircle } from "react-icons/fa";
+
+const Comment = ({ comment }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // Fetch the user's data based on the comment's UserId
+    fetch(`http://localhost:4000/users/${comment.UserId}`)
+      .then((response) => response.json())
+      .then((data) => setUser(data))
+      .catch((error) => console.error("Error fetching user:", error));
+  }, [comment.UserId]);
+  const formatDate = (timestamp) => {
+    const date = new Date(timestamp);
+    return date.toDateString(); // Format the timestamp to display only the date
+  };
+  return (
+    <div className="ml-4 border-l-2 pl-4 mt-4">
+      <div className="flex items-center mb-1">
+        {user && user.image_URL ? (
+          <img
+            className="w-8 h-8 rounded-full mr-2"
+            src={user.image_URL}
+            alt={`Avatar of ${user.userName}`}
+          />
+        ) : (
+          <FaUserCircle className="w-8 h-8 mr-2 text-gray-500" />
+        )}
+        <div>
+        {user && <span className="font-semibold">{user.userName}</span>}
+  
+      </div>
+      </div>
+      <p className="text-gray-600">{comment.content}</p>
+      {comment.createdAt && (
+          <span className="text-gray-500 text-xs">{formatDate(comment.createdAt)}</span>
+        )}
+      {comment.replies && comment.replies.length > 0 && (
+        <div className="mt-2">
+          {comment.replies.map((reply) => (
+            <Comment key={reply.id} comment={reply} />
+          ))}
+    
+        </div>
+      )}
+
+    </div>
+  );
+};
 
 const ViewPost = () => {
   const router = useRouter();
@@ -27,15 +73,26 @@ const ViewPost = () => {
 
   return (
     <RootLayout>
-   <div key={post.id} className="bg-white p-4 rounded-md shadow-md w-2/3 pr-5 my-6 ml-10 flex flex-col sm:flex-col md:flex-col justify-start" > 
-          <div className="flex flex-col justify-center mb-4">
-            <h2 className="text-xl font-semibold">{post.title}</h2>
-            {/* Add the post details, e.g., author, date, etc., here */}
-          </div>
-          <p className="text-gray-600">{post.content}</p>
-          <div className="flex items-center mt-4">
-          </div>
+      <div
+        key={post.id}
+        className="bg-white p-4 rounded-md shadow-md w-2/3 pr-5 my-6 ml-10 flex flex-col sm:flex-col md:flex-col justify-start"
+      >
+        <div className="flex flex-col justify-center mb-4">
+          <h2 className="text-xl font-semibold">{post.title}</h2>
+          {/* Add the post details, e.g., author, date, etc., here */}
         </div>
+        <p className="text-gray-600">{post.content}</p>
+        <div className="flex items-center mt-4">
+          {/* Render the comments */}
+          {post.comments && post.comments.length > 0 && (
+            <div>
+              {post.comments.map((comment) => (
+                <Comment key={comment.id} comment={comment} />
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
     </RootLayout>
   );
 };
