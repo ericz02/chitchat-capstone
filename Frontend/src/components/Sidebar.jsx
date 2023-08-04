181153bf39e506ac2fbdf5351210b640030fdaa2
reo113 <roberteliortiz@gmail.com>
2023-08-03 23:29:41 -0400

    got the link to work to the correct post

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Sidebar = () => {
   return (
-    <div className="flex-none bg-[#27374D] w-1/6 p-4 fixed h-full z-20">
+    <div className=" bg-[#27374D] w-1/6 min-w-[120px] p-4 fixed h-full z-20 overflow-auto">
       <div className="flex flex-col items-center justify-center">
         <div className="mb-8">
           <Link href="/">
@@ -21,26 +21,26 @@ const Sidebar = () => {
 
         <div className="flex flex-col items-left justify-center space-y-4">
          <Link href="/">
-            <div className="flex items-center mt-4 ml-4 text-[24px] text-white self-start">
+            <div className="flex items-center mt-4 ml-4 text-[20px] text-white self-start">
               <FaHome style={{ marginRight: "8px" }} size={24} /> Home{" "}
             </div>
           </Link>
 
           <Link href="/settings">
-            <div className="flex items-center mt-4   ml-4 text-[24px] text-white self-start">
+            <div className="flex items-center mt-4   ml-4 text-[20px] text-white self-start">
               <FaCog style={{ marginRight: "8px" }} size={24} /> Settings{" "}
             </div>
           </Link>
 
           <Link href="/contact">
-            <div className="flex items-center mt-4 ml-4 text-[24px] text-white self-start">
+            <div className="flex items-center mt-4 ml-4 text-[20px] text-white self-start">
               <FaMailBulk style={{ marginRight: "8px" }} size={24} /> Contact Us{" "}
             </div>
           </Link>
         </div>
 
         {/* TODO: Wrap in link component to chitchatrooms */}
-        <div className="flex items-center mt-36 text-[20px] text-white self-center">
+        <div className="flex items-center mt-16 text-[20px] text-white self-center">
           <FaComments style={{ marginRight: "8px" }} size={24} /> ChitChatRooms{" "}
         </div>
       </div>
--- Frontend/src/pages/ViewPost.jsx
+++ Frontend/src/pages/ViewPost.jsx
@@ -1,15 +1,41 @@
+'use client'
 
-
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 const ViewPost = () => {
-  
+  const router = useRouter();
+  const { id } = router.query; // This will get the post ID from the URL
+
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    if (id) {
+      // Fetch post from the server based on the post ID
+      fetch(`http://localhost:4000/posts/${id}`)
+        .then((response) => response.json())
+        .then((data) => setPost(data))
+        .catch((error) => console.error("Error fetching post:", error));
+    }
+  }, [id]);
+
+  if (!post) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
       <p className="bg-[#FFFFFF] h-[2000px] p-4 flex flex-col">
         this is the page to view posts
       </p>
+      {/* Display the post content */}
+      <div>
+        <h2>{post.title}</h2>
+        <p>{post.content}</p>
+      </div>
     </div>
   );
 };
 
 export default ViewPost;
+
