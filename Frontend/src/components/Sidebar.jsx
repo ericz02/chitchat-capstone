5e2e5082383b89880c32b366a55576d06529848a
ericz02 <ezheng0720@gmail.com>
2023-08-07 16:25:15 -0400

    layout fix

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -21,20 +21,20 @@ const Sidebar = () => {
         </div>
 
         <div className="flex flex-col items-left justify-center space-y-4">
-         <Link href="/">
-            <div className="flex items-center mt-4 ml-4 text-[20px] text-white self-start">
+          <Link href="/">
+            <div className="flex items-center mt-2 ml-4 text-[20px] text-white self-start">
               <FaHome style={{ marginRight: "8px" }} size={24} /> Home{" "}
             </div>
           </Link>
 
           <Link href="/settings">
-            <div className="flex items-center mt-4   ml-4 text-[20px] text-white self-start">
+            <div className="flex items-center mt-2   ml-4 text-[20px] text-white self-start">
               <FaCog style={{ marginRight: "8px" }} size={24} /> Settings{" "}
             </div>
           </Link>
 
           <Link href="/contact">
-            <div className="flex items-center mt-4 ml-4 text-[20px] text-white self-start">
+            <div className="flex items-center mt-2 ml-4 text-[20px] text-white self-start">
               <FaMailBulk style={{ marginRight: "8px" }} size={24} /> Contact Us{" "}
             </div>
           </Link>
@@ -43,21 +43,21 @@ const Sidebar = () => {
         {/* TODO: Wrap in link component to chitchatrooms */}
         <div className="flex items-center mt-16 text-[20px] text-white self-center">
           <FaComments style={{ marginRight: "8px" }} size={24} /> ChitChatRooms{" "}
-
         </div>
-        <Link href="/createChatroom">
+        <div className="mt-4">
+          <Link href="/createChatroom">
             <button
-              className="bg-[#E6E6E6] text-black px-4 py-1 rounded-[10px] hover:bg-[#526D82] transition-colors 
-              duration-300 ease-in-out mt-3 text-3xl"
+              className="px-2 py-2 text-s text-white bg-[#526D82] rounded-[10px] hover:bg-[#43556B] 
+              transition-colors duration-300 ease-in-out"
             >
-              +
+              + Create Chatroom
             </button>
           </Link>
+        </div>
         <ChatroomList />
       </div>
     </div>
   );
 };
-
 
 export default Sidebar;
