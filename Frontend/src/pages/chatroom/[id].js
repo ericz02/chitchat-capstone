e2d1fb98cd3b2dd7fe3d39f2826841346b25d0a3
Jay Chen <jaychenjjc@gmail.com>
2023-08-07 19:49:58 -0400

    chatroom almost done

--- Frontend/src/pages/chatroom/[id].js
+++ Frontend/src/pages/chatroom/[id].js
@@ -6,61 +6,14 @@ import { FaUserCircle, FaCommentDots, FaThumbsUp } from "react-icons/fa";
 import Link from "next/link";
 
 
-// export const Comment = ({ comment }) => {
-//   const [user, setUser] = useState(null);
-
-//   useEffect(() => {
-//     // Fetch the user's data based on the comment's UserId
-//     fetch(`/api/user/${comment.UserId}`)
-//       .then((response) => response.json())
-//       .then((data) => setUser(data))
-//       .catch((error) => console.error("Error fetching user:", error));
-//   }, [comment.UserId]);
-//   const formatDate = (timestamp) => {
-//     const date = new Date(timestamp);
-//     return date.toDateString(); // Format the timestamp to display only the date
-//   };
-//   return (
-//     <div className="ml-4 border-l-2 pl-4 mt-4">
-//       <div className="flex items-center mb-1">{/*console.log("user: ",user)*/}
-//         {user && user.profilePicture ? (
-//           <img
-//             className="w-8 h-8 rounded-full mr-2"
-//             src={user.profilePicture}
-//             alt={`Avatar of ${user.userName}`}
-//           />
-//         ) : (
-//           <FaUserCircle className="w-8 h-8 mr-2 text-gray-500" />
-//         )}
-//         <div>
-//           {user && <span className="font-semibold">{user.userName}</span>}
-//         </div>
-//       </div>
-//       <p className="text-gray-600">{comment.content}</p>
-//       {comment.createdAt && (
-//         <span className="text-gray-500 text-xs">
-//           {formatDate(comment.createdAt)}
-//         </span>
-//       )}
-//       {comment.replies && comment.replies.length > 0 && (
-//         <div className="mt-2">
-//           {comment.replies.map((reply) => (
-//             <Comment key={reply.id} comment={reply} />
-//           ))}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-
 const ViewChatRoom = () => {
   const router = useRouter();
   const { id } = router.query; // This will get the chatroom ID from the URL
 
   const [posts, setPosts] = useState(null);
   const [chatroom, setChatroom] = useState(null);
-  const [info, setInfo] = useState({});
+  const [info, setInfo] = useState({name:"", description:""});//this object contains {name, description, length}
+  const [postLength, setPostLength] = useState(null);
 
   //fetch all posts from this chatroom
   useEffect(() => {
@@ -69,7 +22,7 @@ const ViewChatRoom = () => {
         .then((response) => response.json())
         .then((data) => {
           setPosts(data);
-          setInfo({length: data.length});
+          setPostLength(data.length);
         })
         .catch((error) => console.error("Error fetching post:", error));
     }
@@ -82,7 +35,7 @@ const ViewChatRoom = () => {
         .then((response) => response.json())
         .then( (data) => {
           setChatroom(data);
-          setInfo({...info, name:data.chatroomName, description: data.chatroomDescription});
+          setInfo({name:data.chatroomName, description: data.chatroomDescription});
         })
         .catch((error) => console.error("Error fetching chatroom:", error));
     }
@@ -90,46 +43,64 @@ const ViewChatRoom = () => {
   
   console.log("here are all the posts in this chatroom: ", posts);
   console.log("this chatrooms information:", chatroom);
-  console.log("info: ", info);
+  console.log("info: ", info, " length: ", postLength);
   
   return (
-    <RootLayout>
-    <div className = "m-10">
-      <div className = "border-black border-2 p-4">
-        <h1>chatroomName:  {info.name}</h1>
-        <h2>description: {info.description}</h2>
-      </div>
+    <RootLayout >
+    <div className = " border-black border-2 m-10  w-4/5 flex-row self-center p-2">
 
-      <div className="ml-[70px]">
+      <div className = " p-4 flex flex-col justify-items-center bg-slate-100			">
+        <h1 className = "flex justify-center	text-5xl py-4">  {info.name}</h1>
+        <div className = "flex justify-center">
+          <button
+              className="bg-green-300		 text-black px-4 py-2 mx-1 mb-4 rounded-[10px] hover:bg-[#526D82] transition-colors 
+              duration-300 ease-in-out "
+            >
+              Join
+            </button>
+            <button
+              className="bg-red-300	 text-black px-4 py-2 mx-1 mb-4 rounded-[10px] hover:bg-[#526D82] transition-colors 
+              duration-300 ease-in-out "
+            >
+              Leave
+            </button>
+        </div>    
+          <div className = "flex justify-center	">
+            <h2 className = "border-black border-2 p-4 bg-cyan-50	 rounded-[10px]	 w-3/4 ">
+              About:  {info.description}
+            </h2>
+          </div>
+      </div>
+   
 
-  {(posts)?(posts.map((post) => (
-  <Link href={`/post/${post.id}`} key={post.id}>
-    <div
-      key={post.id}
-      className="bg-[#DDE6ED] p-4 rounded-md shadow-md w-2/3 pr-5 my-6 ml-10 cursor-pointer"
-    >
-      <div className="font-bold text-[20px]">
-        cc/{}
-      </div>
-      <div className="flex justify-between items-center mb-4">
-        <h2 className="text-xl">{post.title}</h2>
-        <p className="text-[10px]">Posted by | {}</p>
-      </div>
-      <p className="text-gray-600">{post.content}</p>
-      <div className="flex items-center justify-end mt-4">
-        <div className="flex items-center mr-4">
-          <FaThumbsUp className="mr-2" />
-          <p className="text-[13px]">{post.likesCount}</p>
-        </div>
-        <div className="flex items-center">
-          <FaCommentDots className="mr-2" />
-          <p className="text-[13px]">{}</p>
-        </div>
-      </div>
-    </div>
-  </Link>
-))):(<p className="text-xl">no posts</p>)}
-
+      <div className=" p-4    flex-row items-center">
+        {(postLength>0)?(posts.map((post) => (
+          <Link href={`/post/${post.id}`} key={post.id} >
+            <div
+              key={post.id}
+              className="bg-[#DDE6ED] p-4 rounded-md shadow-md   my-6 cursor-pointer "
+            >
+              <div className="font-bold text-[20px]">
+                 cc/{}
+              </div>
+                <div className="flex justify-between items-center mb-4">
+                  <h2 className="text-xl">{post.title}</h2>
+                  <p className="text-[10px]">Posted by | {}</p>
+                </div>
+                <p className="text-gray-600">{post.content}</p>
+                <div className="flex items-center justify-end mt-4">
+                  <div className="flex items-center mr-4">
+                  <FaThumbsUp className="mr-2" />
+                    <p className="text-[13px]">{post.likesCount}</p>
+                </div>
+                <div className="flex items-center">
+                  <FaCommentDots className="mr-2" />
+                  <p className="text-[13px]">{}</p>
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))):(<p className="text-xl">no posts</p>)}
       </div>
 
     </div>
