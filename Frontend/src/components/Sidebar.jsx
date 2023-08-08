1a21eea6fb3409e42887b608cad862725c9bfa49
ericz02 <ezheng0720@gmail.com>
2023-08-08 10:36:41 -0400

    fixed ui & light touch ups

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -22,32 +22,32 @@ const Sidebar = () => {
 
         <div className="flex flex-col items-left justify-center space-y-4">
           <Link href="/">
-            <div className="flex items-center mt-2 ml-4 text-[20px] text-white self-start">
+            <div className="flex items-center mt-2 ml-4 text-[20px] text-white hover:text-gray-400 self-start">
               <FaHome style={{ marginRight: "8px" }} size={24} /> Home{" "}
             </div>
           </Link>
 
           <Link href="/settings">
-            <div className="flex items-center mt-2   ml-4 text-[20px] text-white self-start">
+            <div className="flex items-center mt-2   ml-4 text-[20px] text-white hover:text-gray-400 self-start">
               <FaCog style={{ marginRight: "8px" }} size={24} /> Settings{" "}
             </div>
           </Link>
 
           <Link href="/contact">
-            <div className="flex items-center mt-2 ml-4 text-[20px] text-white self-start">
+            <div className="flex items-center mt-2 ml-4 text-[20px] text-white hover:text-gray-400 self-start">
               <FaMailBulk style={{ marginRight: "8px" }} size={24} /> Contact Us{" "}
             </div>
           </Link>
         </div>
 
         {/* TODO: Wrap in link component to chitchatrooms */}
-        <div className="flex items-center mt-16 text-[20px] text-white self-center">
+        <div className="flex items-center mt-16 text-[20px] text-white hover:text-gray-400 self-center">
           <FaComments style={{ marginRight: "8px" }} size={24} /> ChitChatRooms{" "}
         </div>
         <div className="mt-4">
           <Link href="/createChatroom">
             <button
-              className="px-2 py-2 text-s text-white bg-[#526D82] rounded-[10px] hover:bg-[#43556B] 
+              className="px-2 py-2 text-s text-white hover:text-gray-400 bg-[#526D82] rounded-[10px] hover:bg-[#43556B] 
               transition-colors duration-300 ease-in-out"
             >
               + Create Chatroom
