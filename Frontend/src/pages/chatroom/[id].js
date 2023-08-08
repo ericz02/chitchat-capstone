62f78372e7b728ab9878f516c2bdef83e81a5826
Jay Chen <jaychenjjc@gmail.com>
2023-08-07 21:26:04 -0400

    added join and leave, tried to verify if user was in room

--- Frontend/src/pages/chatroom/[id].js
+++ Frontend/src/pages/chatroom/[id].js
@@ -1,7 +1,7 @@
 "use client";
 import RootLayout from "@/app/layout";
 import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
+import { useState, useEffect,useContext } from "react";
 import { FaUserCircle, FaCommentDots, FaThumbsUp } from "react-icons/fa";
 import Link from "next/link";
 
@@ -9,20 +9,50 @@ import Link from "next/link";
 const ViewChatRoom = () => {
   const router = useRouter();
   const { id } = router.query; // This will get the chatroom ID from the URL
-
   const [posts, setPosts] = useState(null);
   const [chatroom, setChatroom] = useState(null);
   const [info, setInfo] = useState({name:"", description:""});//this object contains {name, description, length}
   const [postLength, setPostLength] = useState(null);
+  const[joined,setJoined] = useState(false);
+  const[role,setRole] = useState(null);
+  //check if the 
+  useEffect(()=>{
+    //check if user is a member of the chatroom
+    if(id){
+      fetch(`/api/chatrooms/isMemberOf/${id}`,{method:"GET"})
+      .then((response) => response.json())
+      .then((data)=>{
+        if(data.there){//if the user is there then set joined to true 
+          setJoined(true);
+          setRole(data.role);
+        }else{
+          setJoined(false);
+        }
+      })
+    }
+  },[joined]);
+  console.log("joined: ",joined, "role: ", role, "id: ",id);
 
   //fetch all posts from this chatroom
   useEffect(() => {
     if (id) {
       fetch(`/api/chatrooms/${id}/posts`,{method:"GET"})
         .then((response) => response.json())
-        .then((data) => {
-          setPosts(data);
+        .then(async (data) => {
           setPostLength(data.length);
+          //fetch user data so that the posts state will contain posts and user who created 
+          const postsWithUserDetails = await Promise.all(
+            data.map(async (post) => {
+              //get user information
+              const userResponse = await fetch(`/api/user/${post.UserId}`, {
+                method: "GET",
+              });
+              const userData = await userResponse.json();
+  
+              return { ...post, user: userData };//spread the post itself, the user data gotten from post.userid, and the chatroom from post.chatroomId
+            })
+          );
+            setPosts(postsWithUserDetails);
         })
         .catch((error) => console.error("Error fetching post:", error));
     }
@@ -49,25 +79,31 @@ const ViewChatRoom = () => {
     <RootLayout >
     <div className = " border-black border-2 m-10  w-4/5 flex-row self-center p-2">
 
-      <div className = " p-4 flex flex-col justify-items-center bg-slate-100			">
+      <div className = " p-4 flex flex-col justify-items-center bg-slate-100">
         <h1 className = "flex justify-center	text-5xl py-4">  {info.name}</h1>
-        <div className = "flex justify-center">
-          <button
-              className="bg-green-300		 text-black px-4 py-2 mx-1 mb-4 rounded-[10px] hover:bg-[#526D82] transition-colors 
-              duration-300 ease-in-out "
-            >
-              Join
-            </button>
+
+          {(joined) ?(
+          <div className = "flex justify-center">
             <button
               className="bg-red-300	 text-black px-4 py-2 mx-1 mb-4 rounded-[10px] hover:bg-[#526D82] transition-colors 
               duration-300 ease-in-out "
             >
               Leave
             </button>
-        </div>    
+          </div> 
+          ):(
+          <div className = "flex justify-center">
+            <button
+                className="bg-green-300		 text-black px-4 py-2 mx-1 mb-4 rounded-[10px] hover:bg-[#526D82] transition-colors 
+                duration-300 ease-in-out "
+              >
+                Join
+            </button>
+          </div>)}
+            {(joined)?(<p>You are a {role} of this Room</p>):(<></>)}
           <div className = "flex justify-center	">
             <h2 className = "border-black border-2 p-4 bg-cyan-50	 rounded-[10px]	 w-3/4 ">
-              About:  {info.description}
+              {info.description}
             </h2>
           </div>
       </div>
@@ -81,11 +117,11 @@ const ViewChatRoom = () => {
               className="bg-[#DDE6ED] p-4 rounded-md shadow-md   my-6 cursor-pointer "
             >
               <div className="font-bold text-[20px]">
-                 cc/{}
+                 cc/{info.name}
               </div>
                 <div className="flex justify-between items-center mb-4">
                   <h2 className="text-xl">{post.title}</h2>
-                  <p className="text-[10px]">Posted by | {}</p>
+                  <p className="text-[10px]">Posted by | {post.user.userName}</p>
                 </div>
                 <p className="text-gray-600">{post.content}</p>
                 <div className="flex items-center justify-end mt-4">
@@ -95,12 +131,12 @@ const ViewChatRoom = () => {
                 </div>
                 <div className="flex items-center">
                   <FaCommentDots className="mr-2" />
-                  <p className="text-[13px]">{}</p>
+                  <p className="text-[13px]">{post.commentsCount}</p>
                 </div>
               </div>
             </div>
           </Link>
-        ))):(<p className="text-xl">no posts</p>)}
+        ))):(<p className="text-xlf flex justify-center	 text-red text-5xl">No Posts</p>)}
       </div>
 
     </div>
