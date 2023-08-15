0d263c747c9bc1ca74154bf28daebcb762246678
Jay Chen <jaychenjjc@gmal.com>
2023-08-15 15:13:47 -0400

    centered foot

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -2,11 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className = " ">
       <div className="bg-[#FFCD29] text-center p-2 text-sm">
         Copyright © 2023 by ChitChat. All rights reserved.
       </div>
-    </div>
   );
 };
 
